23f78ee7fb2886ad1d6178b84a5a68540f690a4f
Dragon_X <barron.samuel111@gmail.com>
2025-03-10 19:37:32 -0700

    fixed

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer");
 
-const launchBrowser = async () => {
+const scrapeLogic = async (res) => {
   const browser = await puppeteer.launch({
     headless: false, // Opens an interactive window
     args: [
@@ -12,9 +12,11 @@ const launchBrowser = async () => {
     const page = await browser.newPage();
     await page.goto("https://math.prodigygame.com", { waitUntil: "networkidle2" });
     console.log("Opened math.prodigygame.com in an interactive Chrome window.");
+    res.send("Browser launched and navigated to math.prodigygame.com");
   } catch (e) {
     console.error("Error launching page:", e);
+    res.send(`Error launching page: ${e}`);
   }
 };
 
-launchBrowser();
+module.exports = { scrapeLogic };
